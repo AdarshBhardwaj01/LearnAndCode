c9a6279f0cfb3f8a2946c5a7470db75ccf34ff7c
AdarshBhardwaj01 <adarsh.k.b@intimetec.com>
2026-03-11 20:28:32 +0530

    Add geocoding service interface to define application boundary

---
+++ GeoLocationBoundaryApp/GeoLocationBoundaryApp/GeoLocationBoundaryApp/Interfaces/IGeocodingService.cs
@@ -0,0 +1,14 @@
+﻿using GeoLocationBoundaryApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLocationBoundaryApp.Interfaces
+{
+    public interface IGeocodingService
+    {
+        Task<List<LocationResult>> GetCoordinatesAsync(string location);
+    }
+}
