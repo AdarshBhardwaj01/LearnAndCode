7d3d8c982c43859484f4cad5d6bb5aa7929cfefc
AdarshBhardwaj01 <adarsh.k.b@intimetec.com>
2026-03-11 20:31:22 +0530

    Add location service and console interaction for retrieving coordinates

---
+++ GeoLocationBoundaryApp/GeoLocationBoundaryApp/GeoLocationBoundaryApp/Services/LocationService.cs
@@ -0,0 +1,26 @@
+﻿using GeoLocationBoundaryApp.Interfaces;
+using GeoLocationBoundaryApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GeoLocationBoundaryApp.Services
+{
+    public class LocationService
+    {
+        private readonly IGeocodingService _geocodingService;
+
+        public LocationService(IGeocodingService geocodingService)
+        {
+            _geocodingService = geocodingService;
+        }
+
+        public async Task<List<LocationResult>> GetLocationAsync(string location)
+        {
+            if (string.IsNullOrWhiteSpace(location))
+                throw new ArgumentException("Location cannot be empty.");
+
+            return await _geocodingService.GetCoordinatesAsync(location);
+        }
+    }
+}
